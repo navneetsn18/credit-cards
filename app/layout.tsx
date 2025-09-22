c6bd248df59c0bc0b4b3f6ea9d00b51aea9d263e
navneetsn18 <navneetsn18@gmail.com>
2025-09-23 02:48:17 +0530

    feat: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <Toaster richColors position="top-right" closeButton theme="light" />
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
